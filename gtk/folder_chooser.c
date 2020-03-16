2f57cf47e5d7432ddf70eadb41f37b41e87158d4
Jeff Stafford <jeff.stafford@protonmail.com>
2020-03-16 01:37:46 -0400

    use native dialog

--- gtk/folder_chooser.c
+++ gtk/folder_chooser.c
@@ -5,21 +5,19 @@
  */
 char *folder_chooser(char *title) {
     gtk_init(NULL, NULL);
-    GtkWindow *window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
-    GtkWidget *dialog = gtk_file_chooser_dialog_new(
-        title, window, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "Cancel",
-        GTK_RESPONSE_CANCEL, "Select", GTK_RESPONSE_ACCEPT, NULL);
+    GtkFileChooserNative *dialog = gtk_file_chooser_native_new(
+        title, NULL, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, NULL, NULL);
     GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
     gtk_file_chooser_set_current_folder(chooser, g_get_home_dir());
 
     char *path = NULL;
-    gint res = gtk_dialog_run(GTK_DIALOG(dialog));
+    gint res = gtk_native_dialog_run(GTK_NATIVE_DIALOG(dialog));
     if (res == GTK_RESPONSE_ACCEPT) {
         path = gtk_file_chooser_get_filename(chooser);
     } else {
         path = strdup("");
     }
-    gtk_widget_destroy(GTK_WIDGET(window));
-    gtk_widget_destroy(dialog);
+    gtk_native_dialog_destroy(GTK_NATIVE_DIALOG(dialog));
+    g_object_unref(dialog);
     return path;
 }
